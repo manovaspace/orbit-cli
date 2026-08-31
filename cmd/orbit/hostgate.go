f355beccbdf173b7bec3436d626e778d11d4ab4d
Admin <admin@manova.space>
2026-08-31 13:27:39 +0000

    feat(hostgate): support ORBIT_TESTBED and ORBIT_SKIP_HOSTGATE overrides

--- cmd/orbit/hostgate.go
+++ cmd/orbit/hostgate.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/manovaspace/orbit-cli/pkg/host"
 	"github.com/spf13/cobra"
 )
 
 func enforceHost(cmd *cobra.Command, live func() host.Report) error {
+	if os.Getenv("ORBIT_TESTBED") == "1" || os.Getenv("ORBIT_SKIP_HOSTGATE") == "1" {
+		return nil
+	}
 	for c := cmd; c != nil; c = c.Parent() {
 		switch c.Name() {
 		case "version", "doctor", "uninstall":
